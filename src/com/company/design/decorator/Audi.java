983d6c64de470fc09001b9ec8faeb30c76247d45
seungmiKim <kmimi0528@gmail.com>
2022-05-21 17:38:52 +0900

    decorator pattern

---
+++ src/com/company/design/decorator/Audi.java
@@ -0,0 +1,22 @@
+package com.company.design.decorator;
+
+public class Audi implements ICar{
+
+    private int cost;
+
+    public Audi(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public int getPrice() {
+        return cost;
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println("audi 의 가격은 "+ this.cost+"원 입니다. ");
+
+
+    }
+}
